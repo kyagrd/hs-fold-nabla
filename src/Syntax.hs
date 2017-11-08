b2dbf7bbdb28226c2199f6623784ae51fd591f69
Ki Yung Ahn <kyagrd@gmail.com>
2017-11-08 17:48:18 +0800

    some more rules written

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE FlexibleInstances         #-}
 {-# LANGUAGE MultiParamTypeClasses     #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
@@ -14,6 +15,7 @@ import           Data.Foldable
 import           Data.Tree
 import           Unbound.LocallyNameless
 import           Unbound.LocallyNameless.Ops
+import           Unbound.LocallyNameless.Types
 
 type Nm = Name Tm
 
@@ -41,7 +43,7 @@ data Form
 
 type Sig = [(Nm,Embed Ty)]
 type Def = (String, Bind Sig ([Tm], Form)) -- definitional clause
-type Judgment = Bind Sig Form
+type Judgment = SetBind Sig Form
 type Sequent = (Sig, [Judgment], Judgment)
 
 instance Eq (Bind Nm Tm) where (==) = aeq
@@ -50,8 +52,8 @@ instance Ord (Bind Nm Tm) where compare = acompare
 instance Eq (Bind (Nm,Embed Ty) Form) where (==) = aeq
 instance Ord (Bind (Nm,Embed Ty) Form) where compare = acompare
 
-instance Eq (Bind Sig Form) where (==) = aeq
-instance Ord (Bind Sig Form) where compare = acompare
+instance Eq (SetBind Sig Form) where (==) = aeq
+instance Ord (SetBind Sig Form) where compare = acompare
 
 
 $(derive [''Tm,''Ty,''Form])
@@ -106,27 +108,90 @@ lv1 lvp (Exists b)   = lv1 lvp (snd $ unsafeUnbind b)
 lv1 lvp (Nabla b)    = lv1 lvp (snd $ unsafeUnbind b)
 
 
+unsafeForm :: Judgment -> Form
+unsafeForm = snd . unsafeUnbind
+
+nab2sig :: Fresh m => Judgment -> m Judgment
+nab2sig g | Nabla _ <- unsafeForm g =
+            do (lsig, Nabla b) <- unbind g
+               (xt0@(_,Embed _), f) <- unbind b
+               pure $ bindSig lsig f
+          | otherwise = pure g
+
+bindSig sig f = permbind (compactSig sig f) f
+
+compactSig sig f = [xt | xt@(x,_) <- sig, x `elem` fvf]
+  where fvf = fv f :: [Nm]
+
 infer :: (Alternative m, Fresh m) => Sequent -> m (Tree Sequent)
 infer sqnt@(sig, gs, g)
+  -- cut, cL, wL ??
   | g `elem` gs                   = return $ Node sqnt [] -- init
   | TT <- unsafeForm g            = return $ Node sqnt [] -- TT-R
   | FF `elem` (unsafeForm <$> gs) = return $ Node sqnt [] -- FF-L
-  -- Nabla-L (local)
-  -- Nabla-R (local)
+  | (not . null) [() | Nabla _ <- unsafeForm <$> g:gs] =  -- Nabla-R, Nabla-L
+    do g':gs' <- mapM nab2sig (g:gs)
+       Node sqnt . (:[]) <$> infer (sig, gs', g')
+  | Forall _ <- unsafeForm g = -- Forall-R (raising)
+    do (lsig, Forall b) <- unbind g
+       (sig', f') <- raise (sig, lsig, b)
+       Node sqnt . (:[]) <$> infer (sig', gs, bindSig lsig f')
+  | (not . null) [() | Exists _ <- unsafeForm <$> gs] = -- Exists-L (raising)
+    do (sig', gs') <- raiseExistsL sig gs
+       Node sqnt . (:[]) <$> infer (sig, gs', g)
+  | Imply _ _ <- unsafeForm g = -- Imply-R
+    do (lsig, Imply g1 g2) <- unbind g
+       Node sqnt . (:[]) <$> infer (sig, bindSig lsig g1 : gs, bindSig lsig g2)
+  | (not . null) [() | Conj _ <- unsafeForm <$> gs] = -- Conj-L
+    do gs' <- conjL gs
+       Node sqnt . (:[]) <$> infer (sig, gs', g)
+
   | Conj _ <- unsafeForm g = -- Conj-R
     do (lsig, Conj fs) <- unbind g
-       Node sqnt <$> mapM infer [(sig,gs,bind lsig f) | f<-fs]
+       Node sqnt <$> mapM infer [(sig,gs,bindSig lsig f) | f<-fs]
+  | (not . null) [() | Disj _ <- unsafeForm <$> gs] = -- Disj-L
+    do gss <- disjL gs
+       Node sqnt <$> mapM infer [(sig, gs', g) | gs' <- gss]
+  -- Imply-L   TODO
+
   | Disj _ <- unsafeForm g = -- Disj-R
     do (lsig, Disj fs) <- unbind g
-       asum [infer (sig,gs,bind lsig f) | f<-fs]
-  -- cut, cL, wL ??
-  -- Conj-L
-  -- Disj-L
-  -- Imply-R
-  -- Imply-L
-  -- Forall-R (raising)
-  -- Exists-L (raising)
+       Node sqnt . (:[]) <$> asum [infer (sig,gs,bindSig lsig f) | f<-fs]
+
   -- Forall-L (solving)
   -- Exists-R (solving)
-  where
-    unsafeForm = snd . unsafeUnbind
+
+-- raise :: Fresh m => (Sig, Sig, Bind (Name Tm, Embed Ty) Form) -> m (Sig, Form)
+raise (sig, lsig, b) =
+  do (xty@(x,Embed xt), f) <- unbind b
+     h <- fresh (s2n "H")
+     let (vs, etys) = unzip lsig
+         tys = [ty | Embed ty <- etys]
+         sig' = (h, Embed $ foldr1 Arr (tys++[xt])) : sig
+         f' = subst x (foldl1 App $ map V (h:vs)) f
+     return (sig, f')
+
+raiseExistsL sig (g:gs)
+  | Exists _ <- unsafeForm g = do (lsig, Exists b) <- unbind g
+                                  (sig1, f') <- raise (sig, lsig, b)
+                                  let g' = bindSig lsig f'
+                                  (sig', gs') <- raiseExistsL sig1 gs
+                                  return (sig', g':gs')
+  | otherwise                = do (sig', gs') <- raiseExistsL sig gs
+                                  return (sig', g:gs')
+raiseExistsL sig []          = return (sig, [])
+
+conjL :: Fresh m => [Judgment] -> m [Judgment]
+conjL (g:gs) | Conj _ <- unsafeForm g =
+               do (lsig, Conj fs) <- unbind g
+                  ([bindSig lsig f | f<-fs] ++) <$> conjL gs
+             | otherwise = (g:) <$> conjL gs
+conjL [] = return []
+
+disjL :: Fresh m => [Judgment] -> m [[Judgment]]
+disjL (g:gs) | Disj _ <- unsafeForm g =
+               do (lsig, Disj fs) <- unbind g
+                  gss <- disjL gs
+                  return [g':gs' | g' <- bindSig lsig <$> fs, gs' <- gss]
+             | otherwise = map (g:) <$> disjL gs
+disjL [] = return [[]]
